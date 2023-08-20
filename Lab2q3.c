fbc74e110becc2d6070cb8dd9305fb75747776dd
Anushka Singh <97422676+singhanushka0214@users.noreply.github.com>
2023-08-20 18:04:55 +0530

    Create Lab2q3.c

---
+++ Lab2q3.c
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Internal</title>
+        <style>
+            h1{
+                color:red;
+            }
+        </style>
+    </head>
+    <body>
+        <h1><center><u>KIIT UNIVERSITY</u></center></h1>
+    </body>
+</html>
